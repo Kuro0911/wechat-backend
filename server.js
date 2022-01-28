8660349be592fb94c4a324da41e537ebd703780f
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-01-28 18:44:45 +0530

    fixes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
 import Pusher from "pusher";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -15,12 +16,7 @@ const pusher = new Pusher({
 });
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-});
+app.use(cors());
 
 const connect_url =
   "mongodb+srv://Kuro:ao7Jq6QRDrZnSDj8@cluster0.p3xln.mongodb.net/WechatDB?retryWrites=true&w=majority";
